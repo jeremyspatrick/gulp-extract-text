a8c922c018ac8648907f1fee1e906a58d57c19a7
Jeremy <jeremy.scott.patrick@gmail.com>
2021-11-22 15:24:42 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -6,9 +6,9 @@ module.exports = function (params, options) {
     let { pattern_start, pattern_end } = params;
     return through2.obj(function (file, enc, cb) {
         let contents  = file.contents.toString('utf8');
-        let pattern   = new RegExp(`${pattern_start}((.|\\n)*?)${pattern_end}`);
+        let pattern   = new RegExp(`${pattern_start}((.|\\r\\n)*?)${pattern_end}`);
         let extract   = pattern.exec(contents);
-        extract       = extract ? extract[1] : '';
+        extract       = extract ? extract[1] : 'null';
         file.contents = new Buffer(extract);
         return cb(null, file);
     });
